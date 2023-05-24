3d4c3653361754487b2102bfe11b183c2fc4ef10
Thinh Phan <pseudothinhphan@gmail.com>
2023-05-23 20:25:24 -0700

    Implement displayBook function
    Display all the info of any new book added by
    users.

--- js/library.js
+++ js/library.js
@@ -1,5 +1,6 @@
 let myLibrary = [];
 const addBook = document.querySelector('button');
+const bookDisplay = document.querySelector('#display');
 
 function Book(author, title, pages, read) {
     this.author = author;
@@ -19,4 +20,13 @@ function addBookToLibrary() {
     let newBook = new Book(author, title, pages, read);
 
     myLibrary.push(newBook);
+    displayBook(newBook);
+}
+
+function displayBook(book) {
+    let newBookDisplay = document.createElement('div');
+
+    newBookDisplay.textContent = `Title: ${book.title}, Author: ${book.author}, Pages: ${book.pages}, ${book.read}`;
+
+    bookDisplay.append(newBookDisplay);
 }
