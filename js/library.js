dc3e181745bbad00f2498d68d64ec742f561399b
Thinh Phan <pseudothinhphan@gmail.com>
2023-05-24 17:54:40 -0700

    Add "Read or not" option to the form

--- js/library.js
+++ js/library.js
@@ -3,10 +3,11 @@ let myLibrary = [];
 const bookDisplay = document.querySelector('#display');
 const bookForm = document.querySelector('form');
 
-function Book(author, title, pages) {
+function Book(author, title, pages, readOrNot) {
     this.author = author;
     this.title = title;
     this.pages = pages;
+    this.readOrNot = readOrNot;
 }
 
 bookForm.addEventListener('submit', addBookToLibrary);
@@ -16,8 +17,12 @@ function addBookToLibrary(event) {
     let author = document.querySelector('#author').value;
     let title = document.querySelector('#title').value;
     let pages = document.querySelector('#pages').value;
+    let read = document.querySelector('#read');
 
-    let newBook = new Book(author, title, pages);
+    read.checked === true ? read.value = "yes" : read.value = "no";
+    let readOrNot = read.value;
+
+    let newBook = new Book(author, title, pages, readOrNot);
 
     myLibrary.push(newBook);
     displayBook(newBook);
@@ -28,7 +33,7 @@ function addBookToLibrary(event) {
 function displayBook(book) {
     let newBookDisplay = document.createElement('div');
 
-    newBookDisplay.textContent = `Author: ${book.author}, Title: ${book.title}, Pages: ${book.pages}`;
+    newBookDisplay.textContent = `Author: ${book.author}, Title: ${book.title}, Pages: ${book.pages}, Read or not: ${book.readOrNot}`;
 
     bookDisplay.append(newBookDisplay);
 }
