a4ed20f8111e62cf5ef84a28b0582bb614dad592
Thinh Phan <pseudothinhphan@gmail.com>
2023-05-31 17:45:37 -0700

    Change yes & no to Read & Not Yet for status

--- js/library.js
+++ js/library.js
@@ -1,8 +1,8 @@
 let myLibrary = [
-    {title: 'Brain Rules', author: 'John Medina', pages: 301, status: 'yes'},
-    {title: 'The Dip', author: 'Seth Godin', pages: 96, status: 'no'},
-    {title: 'Starting Strength', author: 'Mark Rippetoe', pages: 347, status: 'yes'},
-    {title: 'The Obstacle is the Way', author: 'Ryan Holiday', pages: 224, status: 'yes'}
+    {title: 'Brain Rules', author: 'John Medina', pages: 301, status: 'Read'},
+    {title: 'The Dip', author: 'Seth Godin', pages: 96, status: 'Not Yet'},
+    {title: 'Starting Strength', author: 'Mark Rippetoe', pages: 347, status: 'Read'},
+    {title: 'The Obstacle is the Way', author: 'Ryan Holiday', pages: 224, status: 'Read'}
 ];
 
 const bookDisplay = document.querySelector('#display');
@@ -28,10 +28,15 @@ function addBookToLibrary(event) {
     let title = document.querySelector('#title').value;
     let pages = document.querySelector('#pages').value;
     let read = document.querySelector('#read');
-
-    read.checked === true ? read.value = "yes" : read.value = "no";
     let status = read.value;
-
+    
+    read.checked === true ? read.value = 'yes' : read.value = 'no';
+    if (read.value === 'yes') { 
+        status = 'Read';
+    } else if (read.value === 'no') {
+        status = 'Not Yet'; 
+    }
+    
     let newBook = new Book(author, title, pages, status);
 
     myLibrary.push(newBook);
@@ -45,7 +50,6 @@ function createNewBookIndex(lib, order) {
     let newTitle = document.createElement('div');
     let newAuthor = document.createElement('div');
     let newPages = document.createElement('div');
-    let newRead = document.createElement('div');
     let switchBtn = document.createElement('button');
     let removeBtn = document.createElement('button');
 
@@ -53,9 +57,7 @@ function createNewBookIndex(lib, order) {
     newAuthor.textContent = `Author: ${lib.author}`;
     newPages.textContent = `Pages: ${lib.pages}`;
 
-    newRead.textContent = `Status: ${lib.status}`;
-    newRead.append(switchBtn);
-    switchBtn.textContent = 'Switch';
+    switchBtn.textContent = `${lib.status}`;
     switchBtn.addEventListener('click', changeBookStatus);
 
     removeBtn.textContent = 'Delete';
@@ -65,7 +67,7 @@ function createNewBookIndex(lib, order) {
     newBookIndex.dataset.indexNumber = order;
     newBookIndex.classList.add('book');
     
-    newBookIndex.append(newTitle, newAuthor, newPages, newRead, removeBtn);
+    newBookIndex.append(newTitle, newAuthor, newPages, switchBtn, removeBtn);
 
     return newBookIndex;
 }
@@ -90,21 +92,21 @@ function updateLibraryRemove(oldLibraryIndex) {
 }
 
 function changeBookStatus() {
-    let bookStatus = this.previousSibling;
+    // let bookStatus = this.previousSibling;
 
-    if (bookStatus.textContent.includes('yes')) {
-        bookStatus.textContent = bookStatus.textContent.replace('yes', 'no');
-        changeLibraryStatus(this.parentElement.parentElement, bookStatus);
-    } else if (bookStatus.textContent.includes('no')) {
-        bookStatus.textContent = bookStatus.textContent.replace('no', 'yes');
-        changeLibraryStatus(this.parentElement.parentElement, bookStatus);
+    if (this.textContent.includes('Read')) {
+        this.textContent = this.textContent.replace('Read', 'Not Yet');
+        changeLibraryStatus(this.parentElement, this);
+    } else if (this.textContent.includes('Not Yet')) {
+        this.textContent = this.textContent.replace('Not Yet', 'Read');
+        changeLibraryStatus(this.parentElement, this);
     }
 }
 
 function changeLibraryStatus(book, status) {
-    if (status.textContent.includes('no')) {
-    myLibrary[book.dataset.indexNumber].status = 'no';
-    } else if (status.textContent.includes('yes')) {
-    myLibrary[book.dataset.indexNumber].status = 'yes';
+    if (status.textContent.includes('Not Yet')) {
+    myLibrary[book.dataset.indexNumber].status = 'Not Yet';
+    } else if (status.textContent.includes('Read')) {
+    myLibrary[book.dataset.indexNumber].status = 'Read';
     }
 }
